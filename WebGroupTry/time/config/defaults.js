e00e90d0679b5e35e3e96fc653a0166c943863f6
njohanley <57408454+njohanley@users.noreply.github.com>
2020-03-25 16:41:54 -0300

    Update defaults.js

--- WebGroupTry/time/config/defaults.js
+++ WebGroupTry/time/config/defaults.js
@@ -18,7 +18,7 @@
 define({
   //Default configuration settings for the application. This is where you'll define things like a bing maps key,
   //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
-  "appid": "",
+  "appid": "ea9edba1a67e4de085ce8153cbdc2d40",
   "webmap": "ea9edba1a67e4de085ce8153cbdc2d40",
   "oauthappid": null, //"AFTKRmv16wj14N3z",
   //Group templates must support a group url parameter. This will contain the id of the group.
